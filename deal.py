6bd7b1a6876ab0a6c879dd53e6c045cf2c97ad90
gimpei <gim.kobayashi@gmail.com>
2017-05-11 21:37:14 +0900

    a

--- deal.py
+++ deal.py
@@ -19,7 +19,9 @@ def step1():
       if date_contexts.get(date) is None: 
         date_contexts[date] = ""
       date_contexts[date] += " " + context
-  open("date_contexts.pkl", "wb").write(pickle.dumps(date_contexts)) 
+  """ contextsが5124語になるように調整 """
+  date_contexts = { date: contexts[:5124] for date, contexts in date_contexts.items() }
+  open("var/date_contexts.pkl", "wb").write(pickle.dumps(date_contexts)) 
 
 """ 単語をindex化する """
 def step2():
@@ -33,12 +35,12 @@ def step2():
       for term in terms:
         if term_index.get(term) is None:
           term_index[term] = len(term_index)
-  open("term_index.pkl", "wb").write(pickle.dumps(term_index)) 
+  open("var/term_index.pkl", "wb").write(pickle.dumps(term_index)) 
 
 """ 日経平均を読み取って 平均値を計算して目的関数を出す """
 def step3():
   date_kpi = {}
-  with open("nikkei_mean.csv") as f:
+  with open("var/nikkei_mean.csv") as f:
     for line in f:
       es = iter(re.split(r"\t{1,}", line.strip().replace(" ", "")) )
       date  = next(es)
@@ -47,23 +49,23 @@ def step3():
       minn  = float(next(es))
       date_kpi[date] = (maxx + minn)/2
 
-    open("date_kpi.pkl", "wb").write(pickle.dumps(date_kpi))
+    open("var/date_kpi.pkl", "wb").write(pickle.dumps(date_kpi))
   print("正常に終了しました")
 
 
 """ xgboostの形式に変換する, 予想も目的地も同一を使う """
 def step4():
-  date_kpi      = pickle.loads(open("date_kpi.pkl", "rb").read())
-  date_contexts = pickle.loads(open("date_contexts.pkl", "rb").read())
-  term_index    = pickle.loads(open("term_index.pkl", "rb").read())
+  date_kpi      = pickle.loads(open("var/date_kpi.pkl", "rb").read())
+  date_contexts = pickle.loads(open("var/date_contexts.pkl", "rb").read())
+  term_index    = pickle.loads(open("var/term_index.pkl", "rb").read())
 
-  with open("xgboost.svm", "w") as f:
+  with open("var/xgboost.svm", "w") as f:
     for date, kpi in sorted(date_kpi.items(), key=lambda x:x[0]):
       print(date, kpi)
       id_freq = {}
       try:
         for term, freq in Counter(date_contexts[date].split()).items():
-          id_freq[ term_index[term] ] = math.log(freq) # adhoc
+          id_freq[ term_index[term] ] = math.log(freq + 1) # adhoc
       except KeyError as e:
         print(e)
         continue
@@ -74,11 +76,11 @@ def step4():
 
 """ xgboostの形式に変換する, 予想も目的地も同一を使う """
 def step4alt():
-  date_kpi      = pickle.loads(open("date_kpi.pkl", "rb").read())
-  date_contexts = pickle.loads(open("date_contexts.pkl", "rb").read())
-  term_index    = pickle.loads(open("term_index.pkl", "rb").read())
+  date_kpi      = pickle.loads(open("var/date_kpi.pkl", "rb").read())
+  date_contexts = pickle.loads(open("var/date_contexts.pkl", "rb").read())
+  term_index    = pickle.loads(open("var/term_index.pkl", "rb").read())
 
-  with open("xgboost.svm", "w") as f:
+  with open("var/xgboost.svm", "w") as f:
     for date, kpi in sorted(date_kpi.items(), key=lambda x:x[0]):
       ts        = date.split("-")
       prev_date = "%s-%s-%02d"%( ts[0], ts[1], int(ts[-1]) - 1 )
@@ -91,7 +93,7 @@ def step4alt():
       id_freq = {}
       try:
         for term, freq in Counter(date_contexts[prev_date].split()).items():
-          id_freq[ term_index[term] ] = math.log(freq) # adhoc
+          id_freq[ term_index[term] ] = math.log(freq + 1) # adhoc
       except KeyError as e:
         print(e)
         continue
@@ -102,13 +104,41 @@ def step4alt():
 
 """ トレインとテストに分割する """
 def step5():
-   sets = open("xgboost.svm", "r").read().split("\n")
+   sets = open("var/xgboost.svm", "r").read().split("\n")
    import random
    random.shuffle(sets)
    train = sets[:int(len(sets)*0.8)]
    test  = sets[int(len(sets)*0.8):]
-   open("xgboost.svm.train", "w").write("\n".join(train))
-   open("xgboost.svm.test", "w").write("\n".join(test))
+   open("var/xgboost.svm.train", "w").write("\n".join(train))
+   open("var/xgboost.svm.test", "w").write("\n".join(test))
+
+""" トレインする """
+def step6():
+  os.system("./xgboost.bin var/reg.conf")
+
+""" モデルをダンプする """
+def step7():
+  os.system("./xgboost.bin var/reg.conf task=dump model_in=0300.model name_dump=dump.txt")
+
+""" f値を計算する """
+def step8():
+  term_index = pickle.loads( open("var/term_index.pkl", "rb").read() )
+  index_term = { index:term for term, index in term_index.items() } 
+  fmap = { }
+  for term in open("dump.txt", "r").read().split("\n"):
+    try:
+      f = re.search("\[f(.*?)<", term).group(1)
+      term = index_term[int(f)]
+      if fmap.get(term) is None:
+        fmap[term] = 0.
+      fmap[term] += 1.
+    except Exception as e:
+      #print(e)
+      #print(term)
+      continue
+
+  for term, imp in sorted(fmap.items(), key=lambda x:x[1]*-1):
+    print(term, imp)
 
 if __name__ == '__main__':
   if '--step1' in sys.argv:
@@ -125,5 +155,15 @@ if __name__ == '__main__':
 
   if '--step4alt' in sys.argv:
     step4alt()
+  
   if '--step5' in sys.argv:
     step5()
+
+  if '--step6' in sys.argv:
+    step6()
+
+  if '--step7' in sys.argv:
+    step7()
+
+  if '--step8' in sys.argv:
+    step8()
